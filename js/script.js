cd4013a425dd1ee246ba305b231ae2746ba57033
GraziMonteiro <monteirograziela385@gmail.com>
2021-12-09 19:43:17 -0300

    Projeto

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('.btn').click(function(){
+    $(this).toggleClass("click");
+    $('.sidebar').toggleClass("show");
+   });
+   $('.livros-btn').click(function(){
+    $('nav ul .livros-show').toggleClass("show");
+    $('nav ul .first').toggleClass("rotate");
+   });
+   $('.nov-btn').click(function(){
+    $('nav ul .nov-show').toggleClass("show1");
+    $('nav ul .second').toggleClass("rotate");
+   });
+   $('nav ul li').click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+});
